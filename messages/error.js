5aa651c98aebcff1617577d57367fabf5ae5cc97
Anartz Mugika <mugan86@gmail.com>
2019-03-09 21:10:58 +0100

    Implement multiple 2

--- messages/error.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    /**
-     * Mensaje de error que se ejecuta cuando no tengamos valores numéricos
-     */
-    noNumber: function() {
-        console.log('Un valor o los dos valores no son numéricos');
-    }
-}
